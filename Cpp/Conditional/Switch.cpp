b673343ad02b2496cffc983e2feb7b7a2d8417d4
Nikhil <97503662+nikhilhvr@users.noreply.github.com>
2023-05-30 21:53:13 +0530

    Cpp-Conditional

---
+++ Cpp/Conditional/Switch.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+int number =32;
+
+  switch (number)
+{
+  case 32: cout<<"Number is :-" << number <<endl;
+    break;
+
+
+  case 33: cout <<"Why continue won't work" << endl;
+    break;
+    
+  default:cout << "Default" << endl;
+    break;
+}
+
+
+}
---
+++ Cpp/Conditional/ifElse.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int time = 212;
+  if (time < 101) {
+    cout << "i20.";
+  } else if (time < 22) {
+    cout << "Supra";
+  } else {
+    cout << "Mustang.";
+  }
+  return 0;
+}
